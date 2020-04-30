79b89f7379d041d585e1af46657e1f1a4f64c174
screwyterror83 <screwyterror83@gmail.com>
2020-04-30 00:09:06 -0700

    More to come

--- NOTE.md
+++ NOTE.md
@@ -3,5 +3,23 @@ This is a intro for react.js module
 To start the project, simply use "npx create-react-app app-name" in VSCode.
 All the files in this repo are demo purposes, and can be deleted and modified.
 
-This intro covers some basics of react library, use of JSX and demonstrate how
-front-end can be achieved using react.
+This intro covers some basics of react library, use of JSX and demonstrate how front-end can be achieved using react.
+
+1.  In react app, there will always be a <div id="root"></div> under <body></body> section of "index.html" file, this is where react populate its code.
+
+Below are intro to JSX:
+
+2.  Before writing actual code in "index.js" file, need to import both "react" and "react-dom", commands are:
+    import React from "react";
+    import ReactDOM from "react-dom";
+
+3.  in "index.js"(JSX) file, we can use "ReactDOM.render" to populate code in index.html file, but we can only use .render once, therefore, if you have multiple line of code involves in multiple elements change, you will need to encapsulate all the code in <div></div>.
+
+4.  JSX not only allow us to write html format, it will also allow us to write javascript and to be populated with ReactDOM.render
+
+5.  To insert a "simple javascript expression"/variable in JXS file, simply to use "{}" wrap around the variable/"simple javascript expression", and ReactDOM.render will be able to render within the "{}", however, within the "{}", javascript statements are not allowed.
+  5.1  Template literal: in between JS expression, using `${}` to inject another string. For example: 
+          const fName = "Darren";
+          const lName = "Z"
+          
+          output of {`${fName} ${lName}`} is "Darren Z"
--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"
+
+const name = "Chloe";
+const dadName = "Darren";
+const currentDate = new Date();
+console.log(currentDate)
+const randImg = Math.floor(Math.random()*1100)
+console.log(randImg)
+
+const luckyNumber = Math.floor(Math.random() * 10);
+
+const img = "https://picsum.photos/id/"+ randImg +"/200/200"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <div>
+      <h1 className="heading" spellCheck="false">
+        Hello, {dadName + " and " + name}
+      </h1>
+
+      <h3>I want to tell you something we all know, please pay attention ~!</h3>
+      <p>{name}'s Favorite Foods</p>
+      <div>
+        <img
+          className="food-image"
+          src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/190411-microwaved-bacom-horizontal-2-1555688032.png?crop=0.6668421052631579xw:1xh;center,top&resize=768:*"
+          alt="Bacon"
+        />
+        <img
+          className="food-image"
+          src="https://data.thefeedfeed.com/recommended/15224149465abe35620e896.jpg"
+          alt="Sunny Side Egg"
+        />
+        <img
+          className="food-image"
+          src="https://savorjapan.com/gg/content_image/t0039_002_20180115125708.jpg"
+          alt="Ramen"
+        />
+        <img
+          className="food-image"
+          src="https://assets.bonappetit.com/photos/5c4b21e785b9bd2cf3b17454/16:9/w_2560,c_limit/red-wine-braised-short-ribs.jpg"
+          alt="Braised short Ribs"
+        />
+        <img
+          className="food-image"
+          src="https://cdn.vox-cdn.com/thumbor/BWbLe84vE5po1cSwDO1repgYXYY=/0x0:960x958/920x613/filters:focal(455x381:607x533):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/65724757/hotpot.0.jpg"
+          alt="HotPot"
+        />
+      </div>
+      <p>
+        And {dadName}'s lucky number is: {luckyNumber}
+      </p>
+    </div>
+    <div className="random-image">
+      <h2>
+        &#11015;&#65039; This is a random picture from web, enjoy
+        &#11015;&#65039;
+      </h2>
+      <img src={img} alt="Random" />
+    </div>
+    <div>
+      <p>This page is created by {dadName}</p>
+      <p>Copyright {currentDate.getFullYear()}</p>
+    </div>
+  </div>,
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
